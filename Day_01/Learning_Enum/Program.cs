52f4ab321ccc9a961d0c620088eee0248bed8031
Nihil <35506793+Nihilnia@users.noreply.github.com>
2022-01-02 02:35:38 +0300

    Enum Example

---
+++ Day_01/Learning_Enum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LearningEnum.Roles.GodMode);
+            Console.WriteLine(LearningEnum.Roles.Admin);
+            Console.WriteLine(LearningEnum.Roles.User.GetType());
+
+            Console.Read();
+        }
+    }
+}
