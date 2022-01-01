7d0f52d0e00e89e1219acd68ce4e909c5613bdf2
Nihil <35506793+Nihilnia@users.noreply.github.com>
2022-01-02 02:30:31 +0300

    MD5 Example

---
+++ Day_01/Learning_MD5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_MD5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Username: ");
+                var userInput = Console.ReadLine();
+
+                var result = CryptMD5.EnryptEm(userInput);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
