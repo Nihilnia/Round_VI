8be58eaa54323822bed230f6763615dae29f9429
Nihil <35506793+Nihilnia@users.noreply.github.com>
2022-01-05 06:51:48 +0300

    Area (Admin) added

---
+++ Day_04/movieLib_v2.4/Models/Database/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Faint.Models.Database
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        //public User User { get; set; }
+        public int UserID { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
---
+++ Day_04/movieLib_v2.4/Models/Database/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Faint.Models.Database
+{
+    public class User
+    {
+        public User()
+        {
+            this.Movies = new List<Movie>();
+        }
+
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EMail { get; set; }
+        public string ProfilePic { get; set; }
+        public List<Movie> Movies { get; set; }
+    }
+}
