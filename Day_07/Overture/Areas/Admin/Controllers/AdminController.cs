f651db7a2ad850467468ba185b5d1e466e000d06
Nihil <35506793+Nihilnia@users.noreply.github.com>
2022-01-09 05:13:15 +0300

    JQGrid' s CRUD Operations ok.
    Still have struggles tho.

---
+++ Day_07/Overture/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Overture.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Admin
+        public ActionResult Index()
+        {
+            return View("~/Areas/Admin/Views/Admin/Login.cshtml");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string userName, string passWord)
+        {
+            return View();
+        }
+
+        public ActionResult AddMovie()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
