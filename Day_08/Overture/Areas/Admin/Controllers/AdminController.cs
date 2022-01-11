e36ec76ae0e312a0aae2f71745f7ae388db0bc11
Nihil <35506793+Nihilnia@users.noreply.github.com>
2022-01-11 22:32:34 +0300

    Day 8

---
+++ Day_08/Overture/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,91 @@
+﻿using Overture.Areas.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Overture.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            var model = AdminDatabaseProcesses.GetAdmin(Request.Cookies["fukinAdmin"].Value);
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginControl(string userName, string passWord)
+        {
+
+            var result = AdminDatabaseProcesses.LoginControl(userName, passWord);
+
+            if (result)
+            {
+                HttpCookie mfCookie = new HttpCookie("fukinAdmin", userName);
+                Response.Cookies.Add(mfCookie);
+
+                var model = AdminDatabaseProcesses.GetAdmin(userName);
+                return View("~/Areas/Admin/Views/Admin/Login.cshtml", model);
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Check your login info mf.";
+                return View("~/Areas/Admin/Views/Admin/Failed.cshtml");
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterControl(string userName, string passWord)
+        {
+            var cryptedPass = Crypt.Gloria(passWord);
+
+            var result = AdminDatabaseProcesses.Register(userName, cryptedPass, (int)Roles.RoleTypez.Admin);
+
+            if (result)
+            {
+                var model = AdminDatabaseProcesses.GetAdmin(userName);
+                return View("~/Areas/Admin/Views/Admin/Index.cshtml", model);
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Somethings went wrong.";
+                return View("~/Areas/Admin/Views/Admin/Failed.cshtml");
+            }
+        }
+
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+
+        public ActionResult AddMovie()
+        {
+            return View();
+        }
+
+
+        public ActionResult DeleteProfile()
+        {
+            return View();
+        }
+    }
+}
---
+++ Day_08/Overture/Areas/Database/AdminDatabaseProcesses.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Overture.Areas.Database
+{
+    public static class AdminDatabaseProcesses
+    {
+        public static User GetAdmin(string daMFAdmin)
+        {
+            var aooof = new User();
+
+            using (var DB = new OvertureContext())
+            {
+                var findAdmin = DB.Users.Where(f => f.UserName == daMFAdmin).FirstOrDefault();
+
+                if (findAdmin != null)
+                {
+                    aooof = findAdmin;
+                }
+            }
+
+            return aooof;
+        }
+
+        public static bool Register(string daAdminName, string daAdminPass, int daRole)
+        {
+            bool result = false;
+
+            using (var DB = new OvertureContext())
+            {
+                var cryptedPass = Crypt.Gloria(daAdminPass);
+
+                var newAdmin = new User()
+                {
+                    UserName = daAdminName,
+                    Password = cryptedPass,
+                    Role = daRole
+                };
+
+                DB.Users.Add(newAdmin);
+                DB.SaveChanges();
+
+                result = true;
+            }
+
+            return result;
+        }
+
+
+        public static bool LoginControl(string daAdminName, string daPassWord)
+        {
+            var result = false;
+
+            using (var DB = new OvertureContext())
+            {
+
+                var findAdmin = DB.Users.Where(f => f.UserName == daAdminName).FirstOrDefault();
+
+                if (findAdmin != null)
+                {
+                    var cryptedPass = Crypt.Gloria(daPassWord);
+                    //var crypt_2 = Crypt.Gloria("666");
+
+                    if (findAdmin.Password == cryptedPass)
+                    {
+                        result = true;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static bool Update(User incomingInfos)
+        {
+            bool result = false;
+
+            using (var DB = new OvertureContext())
+            {
+                //
+            }
+
+            return result;
+        }
+
+        public static void Delete()
+        {
+            //
+        }
+    }
+}
